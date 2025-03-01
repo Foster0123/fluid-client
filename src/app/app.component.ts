d97ae3c861a270ed204d2007f370e8126dda649c
Foster Z <fosterunlimited0123@gmail.com>
2025-03-01 18:41:08 +0530

    feat(Import): add navbar to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon'
+import { NavbarComponent } from "./components/navbar/navbar.component";
 @Component({
   selector: 'app-root',
   imports: [
     MatButtonModule,
-    MatIconModule
-  ],
+    MatIconModule,
+    NavbarComponent,
+    RouterOutlet
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'fluid-client';
+  title = 'Fluid';
 }
