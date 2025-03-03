93b41390ee133b001078a76a48a5caf083872186
Foster Z <fosterunlimited0123@gmail.com>
2025-03-03 16:21:46 +0530

    feat(chat-item): style chat-item component

--- src/app/components/chat-item/chat-item.component.ts
+++ src/app/components/chat-item/chat-item.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-chat-item',
-  imports: [],
+  imports: [MatButtonModule, MatCardModule, MatIconModule, FontAwesomeModule],
   templateUrl: './chat-item.component.html',
   styleUrl: './chat-item.component.scss'
 })
 export class ChatItemComponent {
-
+  faCircle = faCircle
 }
