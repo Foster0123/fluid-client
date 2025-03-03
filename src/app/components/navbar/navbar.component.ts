9259a54bc20a894b4f15ccfe75bb1c8e59bfa0af
Foster Z <fosterunlimited0123@gmail.com>
2025-03-03 16:23:34 +0530

    feat(navbar): create mobile responsive version of navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import{ faHamburger } from '@fortawesome/free-solid-svg-icons'
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, FontAwesomeModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
 }
